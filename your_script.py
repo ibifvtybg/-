a64f7db575037323fc289480a4af65de8e423540
ibifvtybg <163284661+ibifvtybg@users.noreply.github.com>
2024-10-01 15:25:29 +0800

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -136,10 +136,10 @@ if st.button("Predict"):
     st.write(f"explainer.expected_value: {explainer.expected_value}")
     
     def generate_shap_plot():
-    shap_plot = shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names))
-    fig = plt.gcf()
-    fig.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
-    return "shap_force_plot.png"
+        shap_plot = shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names))
+        fig = plt.gcf()
+        fig.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
+        return "shap_force_plot.png"
 
     st.title("SHAP Force Plot")
     image_path = generate_shap_plot()
