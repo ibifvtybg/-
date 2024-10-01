40d745c8eaa71282b23ca1860722b9578fd42e6a
ibifvtybg <163284661+ibifvtybg@users.noreply.github.com>
2024-10-01 14:01:16 +0800

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -131,7 +131,7 @@ if st.button("Predict"):
     shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
     
 
-    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names))
+    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names,matplotlib=True))
     plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
 
     st.image("shap_force_plot.png")
